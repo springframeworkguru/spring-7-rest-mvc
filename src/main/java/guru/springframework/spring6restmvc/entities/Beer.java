a70d916bac4a21f0e6cdd7a9fc022559a236c966
teooi <phuc0985531639@gmail.com>
2025-03-08 22:33:23 +0700

    94. Creating JPA Entities

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6restmvc.entities;
+
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beer {
+
+    @Id
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
