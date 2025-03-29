07d33a2ef2b25f9e6f340cc6a36bc0767023ddab
teooi <phuc0985531639@gmail.com>
2025-03-29 16:58:28 +0700

    [phucpn1][2025-03-29] Done CSV Data

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring6restmvc.services;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import guru.springframework.spring6restmvc.model.BeerCSVRecord;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pn1
+ */
+@Service
+public class BeerCsvServiceImpl implements BeerCsvService {
+    @Override
+    public List<BeerCSVRecord> convertCSV(File csvFile) {
+
+        try {
+            List<BeerCSVRecord> beerCSVRecords = new CsvToBeanBuilder<BeerCSVRecord>(new FileReader(csvFile))
+                    .withType(BeerCSVRecord.class)
+                    .build().parse();
+            return beerCSVRecords;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
